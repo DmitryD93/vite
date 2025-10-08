8fe2060479a0c6f6ce367552eeb9c8b596d47685
Dmitriy Danilin <ddan.pro@yandex.ru>
2025-10-09 00:11:40 +0400

    add gsap

--- src/scripts/modules.js
+++ src/scripts/modules.js
@@ -11,3 +11,87 @@ import Accordion from '@modules/accordion';
 //         multiple: false
 //     });
 // });
+
+
+//************ GSAP ******************//
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger)
+
+// window.addEventListener('load',() => {
+//     gsap
+//         .timeline({
+//             scrollTrigger: {
+//                 trigger: '.main',
+//                 start: 'top top',
+//                 end: '+=150%',
+//                 pin: true,
+//                 scrub: true,
+//                 markers: true,
+//             },
+//         })
+//         .to('.space-anima__image', {
+//             scale: 2,
+//             z: 350,
+//             transformOrigin: 'center center',
+//             ease: 'power1.inOut',
+//         })
+//
+//         .to('.hero',{
+//             scale: 1.1,
+//             transformOrigin: 'center center',
+//             ease: 'power1.inOut',
+//         }, '<');
+// })
+
+// window.addEventListener('load',() => {
+//     gsap
+//         .timeline({
+//             scrollTrigger: {
+//                 trigger: '.wrapper',
+//                 start: 'top top',
+//                 end: '+=80%',
+//                 pin: true,
+//                 scrub: true,
+//                 markers: true,
+//             },
+//         })
+//         .to('.space-anima__image', {
+//             scale: 1.3,
+//             z: 200,
+//             transformOrigin: 'center center',
+//             ease: 'power1.inOut',
+//         })
+//
+// })
+
+window.addEventListener('load',() => {
+    gsap.set('.space-anima__image', {
+        filter: 'brightness(0.3) saturate(0.3)'
+    });
+    gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: '.wrapper',
+                start: 'top top',
+                end: '+=80%',
+                pin: true,
+                scrub: true,
+                markers: true,
+            },
+        })
+        .to('.space-anima__image', {
+            scale: 1.3,
+            z: 200,
+            transformOrigin: 'center center',
+            ease: 'power1.inOut',
+        })
+        .to('.space-anima__image', {
+            filter: 'brightness(1) saturate(1)',
+            ease: 'power1.inOut',
+        }, '<')
+})
+
